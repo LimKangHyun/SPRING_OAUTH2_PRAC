c41f7b90eac6de4bedbc5d0fec7ba874ab476e0b
limkanghyun <limkh06910691@gmail.com>
2025-04-17 22:38:46 +0900

    Initial commit

---
+++ src/main/java/io/kanghyun/oauth2_prac/Oauth2PracApplication.java
@@ -0,0 +1,13 @@
+package io.kanghyun.oauth2_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2PracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2PracApplication.class, args);
+    }
+
+}
